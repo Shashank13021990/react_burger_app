eea26cbbfa9b72c88da354eb311c00651164946e
Shashank13021990 <shashankyadav.1990@gmail.com>
2019-08-01 23:10:17 +0100

    pusing the burger changes

---
+++ src/hoc/Auxillary/Auxillary.js
@@ -0,0 +1,5 @@
+const auxillary = props => props.children; // anything within auxillary tags where its called are its children. Not where its defined. Here it is defined.
+
+export default auxillary;
+
+// Here you are not importing React from 'react' as you are not using any JSX.
